721e30d340660bb5b7973f1e959f0b1a54da3c7b
Aleksey <busido_na87@mail.ru>
2022-08-17 12:57:37 +0300

    task_47

---
+++ task_47/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №47:Задайте двумерный массив размером mхn,заполненный случайными вещественными числами.
+
+Console.WriteLine("Задача №47");
+Console.WriteLine("Задать размерность массива mхn");
+Console.Write("Введи m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, n];
+Console.WriteLine("Полученный массив: ");
+for (int i = 0; i < m; i++){
+    for (int j = 0; j < n; j++){
+        matrix[i, j] = new Random().NextDouble();
+        Console.Write(Math.Round(matrix[i, j], 1) + "  ");}
+    Console.WriteLine();}
