d2596fd3fe2dd94038193c98a2630201eb436723
Aleksey <busido_na87@mail.ru>
2022-08-17 22:48:28 +0300

    task_50(c проверкой на правильность ввода чисел)

--- task_50/Program.cs
+++ task_50/Program.cs
@@ -5,15 +5,15 @@
 Console.WriteLine("Задача №50");
 Console.Write("Введи номер позиции элемента строки: n = ");
 int n = Convert.ToInt32(Console.ReadLine());
-n = Math.Abs(n); // при вводе отрицательх чисел,чтобы программа не выдавала ошибку.
 Console.Write("Введи номер позиции элемента столбца: m = ");
 int m = Convert.ToInt32(Console.ReadLine());
-m = Math.Abs(m); 
 int [,] matrix = new int [5,5];
 
 FillArrayRandomNumbers(matrix);
 if (n > matrix.GetLength(0) || m > matrix.GetLength(1)){
     Console.WriteLine("Такого элемента в массиве нет");}
+if (n < 0 || m < 0){
+    Console.WriteLine("Элемента с таким индексом нет");}
 else{
     Console.WriteLine("Полученный массив: ");
     PrintArray(matrix);
